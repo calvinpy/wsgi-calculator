d5c41eb4628f5de4822f8d01e8b0244839deeb7a
calvin fannin <calvin.fannin@gmail.com>
2020-04-18 15:36:12 -0700

    updates to calculator.py

--- calculator.py
+++ calculator.py
@@ -1,45 +1,7 @@
-"""
-For your homework this week, you'll be creating a wsgi application of
-your own.
-
-You'll create an online calculator that can perform several operations.
-
-You'll need to support:
-
-  * Addition
-  * Subtractions
-  * Multiplication
-  * Division
-
-Your users should be able to send appropriate requests and get back
-proper responses. For example, if I open a browser to your wsgi
-application at `http://localhost:8080/multiple/3/5' then the response
-body in my browser should be `15`.
-
-Consider the following URL/Response body pairs as tests:
-
-```
-  http://localhost:8080/multiply/3/5   => 15
-  http://localhost:8080/add/23/42      => 65
-  http://localhost:8080/subtract/23/42 => -19
-  http://localhost:8080/divide/22/11   => 2
-  http://localhost:8080/               => <html>Here's how to use this page...</html>
-```
-
-To submit your homework:
-
-  * Fork this repository (Session03).
-  * Edit this file to meet the homework requirements.
-  * Your script should be runnable using `$ python calculator.py`
-  * When the script is running, I should be able to view your
-    application in my browser.
-  * I should also be able to see a home page (http://localhost:8080/)
-    that explains how to perform calculations.
-  * Commit and push your changes to your fork.
-  * Submit a link to your Session03 fork repository!
-
-
-"""
+import math
+import operator
+import re
+import traceback
 
 
 def add(*args):
@@ -47,11 +9,46 @@ def add(*args):
 
     # TODO: Fill sum with the correct value, based on the
     # args provided.
-    sum = "0"
+    answer = sum(map(int, args))
+    return str(answer)
 
-    return sum
 
-# TODO: Add functions for handling more arithmetic operations.
+def subtract(*args):
+    """ Returns a STRING with the sum of the arguments """
+
+    # TODO: Fill sum with the correct value, based on the
+    # args provided.
+    answer = int(args[0])
+    for item in args[1:]:
+        answer -= int(item)
+    return str(answer)
+
+
+def multiply(*args):
+    """ Returns a STRING with the sum of the arguments """
+
+    # TODO: Fill sum with the correct value, based on the
+    # args provided.
+    answer = 1
+    for item in args:
+        answer *= int(item)
+    return str(answer)
+
+
+def divide(*args):
+    """ Returns a STRING with the sum of the arguments """
+
+    # TODO: Fill sum with the correct value, based on the
+    # args provided.
+    answer = int(args[0])
+    try:
+        for item in args[1:]:
+            answer /= int(item)
+    except ZeroDivisionError:
+        raise ZeroDivisionError
+
+    return str(answer)
+
 
 def resolve_path(path):
     """
@@ -63,10 +60,31 @@ def resolve_path(path):
     # examples provide the correct *syntax*, but you should
     # determine the actual values of func and args using the
     # path.
-    func = add
-    args = ['25', '32']
-
+    funcs = {'': index, 'add': add, 'subtract': subtract, 'multiply': multiply, 'divide': divide}
+    path = path.strip('/').split('/')
+    func_name = path[0]
+    args = path[1:]
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
     return func, args
+
+
+def index():
+    body = ['<h1>Calculator Instructions</h1>', '<ul>']
+    body.append('<h2>Some instructions below:</h2><br>')
+    body.append('Mulitply<br>')
+    body.append('<li>http://localhost:8080/multiply/3/5  => 15</li>')
+    body.append('<br>Add<br>')
+    body.append('<li>http://localhost:8080/add/23/42  => 65</li>')
+    body.append('<br>Subtract<br>')
+    body.append('<li>http://localhost:8080/subtract/23/42  => -19</li>')
+    body.append('<br>Divide<br>')
+    body.append('<li>http://localhost:8080/divide/22/11  => 2</li>')
+    body.append('</ul>')
+    return '\n'.join(body)
+
 
 def application(environ, start_response):
     # TODO: Your application code from the book database
@@ -76,9 +94,33 @@ def application(environ, start_response):
     #
     # TODO (bonus): Add error handling for a user attempting
     # to divide by zero.
-    pass
+    headers = [("Content-type", "text/html")]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except ZeroDivisionError:
+        status = "200 OK"
+        body = "<h1>Can Not divide by zero!</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
+
 
 if __name__ == '__main__':
     # TODO: Insert the same boilerplate wsgiref simple
     # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
